de9ff3865705cc0afe4bf9946c65671bc8702cc8
chen li kuan <clikuan.cs05g@g2.nctu.edu.tw>
2017-07-15 18:14:42 +0800

    add config template

---
+++ Config_template.js
@@ -0,0 +1,13 @@
+exports.privkey = '';
+exports.cert = '';
+exports.ca = '';
+exports.channelAccessToken = '';
+var mailList = [];
+var sendFrom = '';
+var subject = '';
+exports.mail = {
+	from: sendFrom,
+	to: mailList,
+	subject: subject
+}
+exports.port = 8888
