d46cfe481c7e040ab3a0ff491efe030218243464
chen li kuan <clikuan.cs05g@g2.nctu.edu.tw>
2017-07-15 18:14:05 +0800

    catch the unexpected post

--- LineBot.js
+++ LineBot.js
@@ -15,56 +15,60 @@ https.createServer(options, function(req, res){
 			body += data;
 		});
 		req.on('end', function () {
-			var receiveObj = JSON.parse(body);
-			var client = new line.Client({
-				channelAccessToken: config.channelAccessToken		
-			});
-			var date = new Date(receiveObj.events[0].timestamp).toLocaleString();
-			if(receiveObj.events[0].message.type == 'text'){
-				var mail = JSON.parse(JSON.stringify(config.mail));
-				mail.subject = '[' + date + '] ' + mail.subject;
-				mail.html = receiveObj.events[0].message.text;
-				sendmail(mail, function(err, reply) {
-						console.log(err && err.stack);
-						console.dir(reply);
+			try{
+				var receiveObj = JSON.parse(body);
+				var client = new line.Client({
+					channelAccessToken: config.channelAccessToken		
 				});
-			}
-			else if(receiveObj.events[0].message.type == 'image' || receiveObj.events[0].message.type == 'file'){
-				var stream = client.getMessageContent(receiveObj.events[0].message.id);
-				var file = null;
-				stream.on('data', function(data){
-					if(file == null)
-						file = data;
-					else{
-						file =  Buffer.concat([file,data]);
-					}
-				});
-				stream.on('end', function(){
-					var filename;
-					if(receiveObj.events[0].message.type == 'image'){
-						filename = 'image.jpg';
-					}
-					else{
-						filename = receiveObj.events[0].message.fileName;
-					}
+				var date = new Date(receiveObj.events[0].timestamp).toLocaleString();
+				if(receiveObj.events[0].message.type == 'text'){
 					var mail = JSON.parse(JSON.stringify(config.mail));
 					mail.subject = '[' + date + '] ' + mail.subject;
-					mail.attachments = [{
-							filename: filename,
-							content: file
-					}];
+					mail.html = receiveObj.events[0].message.text;
 					sendmail(mail, function(err, reply) {
 							console.log(err && err.stack);
 							console.dir(reply);
 					});
-				});
+				}
+				else if(receiveObj.events[0].message.type == 'image' || receiveObj.events[0].message.type == 'file'){
+					var stream = client.getMessageContent(receiveObj.events[0].message.id);
+					var file = null;
+					stream.on('data', function(data){
+						if(file == null)
+							file = data;
+						else{
+							file =  Buffer.concat([file,data]);
+						}
+					});
+					stream.on('end', function(){
+						var filename;
+						if(receiveObj.events[0].message.type == 'image'){
+							filename = 'image.jpg';
+						}
+						else{
+							filename = receiveObj.events[0].message.fileName;
+						}
+						var mail = JSON.parse(JSON.stringify(config.mail));
+						mail.subject = '[' + date + '] ' + mail.subject;
+						mail.attachments = [{
+								filename: filename,
+								content: file
+						}];
+						sendmail(mail, function(err, reply) {
+								console.log(err && err.stack);
+								console.dir(reply);
+						});
+					});
+				}
+				/*var msg = {
+					type: 'text',
+					text: 'hello world'
+				};
+				client.replyMessage(receiveObj.events[0].replyToken, msg)*/
 			}
-			/*var msg = {
-				type: 'text',
-				text: 'hello world'
-			};
-			client.replyMessage(receiveObj.events[0].replyToken, msg)*/
-			
+			catch(err){
+				console.log(err);
+			}
 		});
 	}
 }).listen(config.port);
